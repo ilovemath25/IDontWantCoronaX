0e55189534d684d7aa3da0bb8b6fa126af81d051
ilovemath25 <147607832+ilovemath25@users.noreply.github.com>
2025-08-03 09:35:15 +0800

    modify scene2

--- src/Scene/scene2.py
+++ src/Scene/scene2.py
@@ -1,4 +1,8 @@
 from src.Scene.scene import Scene
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT
+from src.Core.slideSprite import SlideSprite
+import pygame
+import os
 """
 video (transition scene)
 - the character is in a classroom, taking an exam
@@ -9,18 +13,44 @@ video (transition scene)
 class Scene2(Scene):
     def __init__(self):
         super().__init__()
-    
+        self.slides = []
+        self.current_index = 0
+
     def start(self, app):
         print("Scene2 started")
+        slide_images = [pygame.image.load(os.path.join("assets", "scene2", "scene2-"+str(i)+".png")).convert_alpha() for i in range(1, 4)]
+        for image in slide_images:
+            surf = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT)).convert_alpha()
+            surf.blit(image, (0, 0))
+            slide = SlideSprite(surf, duration=4)
+            self.slides.append(slide)
+        if self.slides:
+            app.group.add(self.slides[0])
         self.scene_state = "UPDATE"
-        
+
     def update(self, app):
-        print("Scene2 updating")
-        if True:
+        if not self.slides:
             self.scene_state = "END"
-            
+            return
+
+        current_slide = self.slides[self.current_index]
+
+        if current_slide.state == "done":
+            app.group.remove(current_slide)
+            self.current_index += 1
+
+            if self.current_index < len(self.slides):
+                app.group.add(self.slides[self.current_index])
+            else:
+                self.scene_state = "END"
+        
+
     def end(self, app):
         print("Scene2 ended")
+
+        for slide in self.slides:
+            app.group.remove(slide)
+
         self.scene_state = "END"
         app.GameState = "SCENE3"
         app.AppState = "START"
